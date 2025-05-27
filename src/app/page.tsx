c8be40295a878a2149aa632ee1c2697a06891bdf
Tauheed Darekar <tauheed.darekar.external@tkelevator.com>
2025-05-27 12:51:14 +0530

    Added glows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
     <div className="flex flex-col items-center w-full justify-around min-h-screen bg-cyan-400">
       <div className="grid grid-cols-1 md:grid-cols-[30%_70%] gap-4 p-4 justify-end">
         <div className="w-[300px]">
-          <div className="w-[310px] h-[500px] border-[0.5px] border-black rounded-4xl p-4 bg-cyan-500 shadow-2xl">
-            <SidebarProfile/>
+          <div className="w-[310px] h-[500px] border-[0.5px] border-gray-300 shadow-[0_0_20px_5px_rgba(59,250,246,0.75)] rounded-4xl p-4 bg-cyan-500 shadow-2xl">
+            <SidebarProfile />
           </div>
         </div>
         <div>
-          <div className="w-[800px] h-auto border-[0.5px] border-2 border-gray-600 rounded-4xl p-4 bg-cyan-500 shadow-2xl">
+          <div className="w-[800px] h-auto border-[0.5px] border-2 border-gray-300 shadow-[0_0_20px_5px_rgba(59,250,246,0.75)] rounded-4xl p-4 bg-cyan-500 shadow-2xl">
             
           </div>
         </div>
